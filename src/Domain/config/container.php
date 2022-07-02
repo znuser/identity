8494da2d7b8a0ac7249aa656f5cd38aa7817fe8a
ZnDeveloper <zndev@ya.ru>
2022-07-02 09:39:37 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -4,12 +4,17 @@ use Psr\Container\ContainerInterface;
 use ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface;
 use ZnBundle\User\Domain\Services\AuthService3;
 use ZnBundle\User\Domain\Subscribers\SymfonyAuthenticationIdentitySubscriber;
+use ZnBundle\User\Domain\Entities\IdentityEntity;
 
 return [
     'definitions' => [
-
+//        'ZnCore\Contract\User\Interfaces\Entities\IdentityEntityInterface' => IdentityEntity::class,
+        'ZnCore\Contract\User\Interfaces\Entities\IdentityEntityInterface' => IdentityEntity::class,
     ],
     'singletons' => [
+        'ZnBundle\User\Domain\Interfaces\Services\IdentityServiceInterface' => 'ZnBundle\User\Domain\Services\IdentityService',
+        'ZnBundle\User\Domain\Interfaces\Repositories\IdentityRepositoryInterface' => 'ZnBundle\User\Domain\Repositories\Eloquent\IdentityRepository',
+
         AuthServiceInterface::class => function (ContainerInterface $container) {
             /** @var AuthService3 $authService */
             $authService = $container->get(AuthService3::class);
@@ -24,5 +29,10 @@ return [
             ]);
             return $authService;
         },
+    ],
+    'entities' => [
+        'ZnCore\Contract\User\Interfaces\Entities\IdentityEntityInterface' => 'ZnBundle\User\Domain\Interfaces\Repositories\IdentityRepositoryInterface',
+//        'ZnCore\Contract\User\Interfaces\Entities\IdentityEntityInterface' => 'ZnBundle\User\Domain\Interfaces\Repositories\IdentityRepositoryInterface',
+        'ZnBundle\User\Domain\Entities\IdentityEntity' => 'ZnBundle\User\Domain\Interfaces\Repositories\IdentityRepositoryInterface',
     ],
 ];
