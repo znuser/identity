1555482a2fc962a2db4c09950a1a6aac6e327508
Vitaliy <theyamshikov@yandex.ru>
2022-07-02 17:34:05 +0600

    upd

---
+++ src/Domain/Events/UserActionEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ZnUser\Identity\Domain\Events;
+
+use Symfony\Contracts\EventDispatcher\Event;
+use ZnCore\Base\EventDispatcher\Traits\EventSkipHandleTrait;
+
+class UserActionEvent extends Event
+{
+
+    use EventSkipHandleTrait;
+
+    private $identityId;
+    private $actionName;
+    private $data;
+
+    public function __construct(int $identityId, string $actionName, $data = null)
+    {
+        $this->identityId = $identityId;
+        $this->actionName = $actionName;
+        $this->data = $data;
+    }
+
+    public function getIdentityId()
+    {
+        return $this->identityId;
+    }
+
+    public function getActionName(): string
+    {
+        return $this->actionName;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+}
